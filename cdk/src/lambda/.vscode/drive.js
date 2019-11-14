e9fe28de3ea8bfb463eebb8b6c7e0acb9fb5e56b
HIRAUCHI <hirauchi.shinichi@classmethod.jp>
2019-11-14 15:09:10 +0900

    drive.js有効化

---
+++ cdk/src/lambda/.vscode/drive.js
@@ -0,0 +1,23 @@
+var event = require(`./${process.argv[2]}`);
+
+
+event.context.System.device.deviceId = 'DEVICE_ID';
+
+
+function resolve(result) {
+    console.log("-------------SUCCESS-------------")
+    //console.log(JSON.stringify(result,undefined,4));
+}
+  
+function reject(error) {
+    console.log("-------------ERROR-------------")
+    console.log(error);
+    console.log(error.stack);
+}
+
+var index = require(`../index.js`);
+index.handler(event, {})
+.then(resolve, reject)
+
+
+
