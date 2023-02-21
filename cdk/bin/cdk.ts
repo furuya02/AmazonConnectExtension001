dd03715494ed36ddc013e014eb79a07d3c08d662
HIRAUCHI <hirauchi.shinichi@classmethod.jp>
2023-02-21 11:23:55 +0900

    cdk version up

--- cdk/bin/cdk.ts
+++ cdk/bin/cdk.ts
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
-import 'source-map-support/register';
-import cdk = require('@aws-cdk/core');
-import { AmazonConnectExtension001Stack } from '../lib/cdk-stack';
+import "source-map-support/register";
+import * as cdk from "aws-cdk-lib";
+import { AmazonConnectExtension001Stack } from "../lib/cdk-stack";
 
 const app = new cdk.App();
-new AmazonConnectExtension001Stack(app, 'AmazonConnectExtension001Stack');
+new AmazonConnectExtension001Stack(app, "AmazonConnectExtension001Stack");
